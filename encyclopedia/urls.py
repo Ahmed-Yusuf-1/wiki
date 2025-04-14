94f870f03a307dc735a7203f004e5826623d9703
Ahmed Yusuf <ahmed.yusuf@edu.sait.ca>
2025-04-14 12:36:57 -0600

    commit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,7 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.create, name="create"),
+    path("randompage", views.randompage, name="randompage"),
     path("<str:name>/edit", views.edit, name="edit"),
     path("<str:name>", views.page, name="page"),
     
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,27 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import util
+import random
+from markdown2 import Markdown
+
 entries = util.list_entries()
 
 def index(request):
     value = None
-   
     if request.method == "POST":
         value = request.POST.get("q")
-        print(entries)
         for entry in entries:
             if value.lower() == entry.lower():
-                return HttpResponseRedirect(reverse("encyclopedia:page", kwargs={"name": value}))
-
+                return HttpResponseRedirect(reverse("encyclopedia:page", kwargs={"name": entry}))
+        
+        # If no exact match, search for substring
+        substring_matches = []
+        for entry in entries:
+            if value.lower() in entry.lower():
+                substring_matches.append(entry)
+        
+        # Render search results page with matches
+        return render(request, "encyclopedia/search_results.html", {
+            "results": substring_matches,
+            "query": value
+        })
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries(), 
+        "entries": entries, 
     })
 
 def page(request, name):
+    markdowner = Markdown()
+    page_content = util.get_entry(name)
+    if page_content == 'Error':
+        return render(request, "encyclopedia/error.html")
     return render(request, "encyclopedia/css.html", {
-        "page": util.get_entry(name), "name": name.capitalize(),
-    })
-
+        "page": markdowner.convert(util.get_entry(name)), "name": name.capitalize(),
+})
 def create(request):
     if request.method == "POST":
         page = request.POST.get("addpage")
@@ -35,7 +50,23 @@ def create(request):
     else:
         return render(request, "encyclopedia/create.html")
     
-def edit(request, page):
+def edit(request, name):
+    if request.method == "POST":
+        editpage = request.POST.get("editpage")
+        editpagedes = request.POST.get("editpagedes")
+        for entry in entries:
+            if entry.lower() == editpage.lower():
+                util.save_entry(editpage, editpagedes)
+                return HttpResponseRedirect(reverse('encyclopedia:page', kwargs={"name": editpage}))
     return render(request, "encyclopedia/edit.html", {
-        "editpage": page
+        "name": name,
+        "pagedes": util.get_entry(name)
+    })
+
+def randompage(request):
+    markdowner = Markdown()
+    ranpage = random.choice(entries)
+    return render(request, "encyclopedia/css.html", {
+        "page": markdowner.convert(util.get_entry(ranpage)),
+        "name": ranpage
     })
